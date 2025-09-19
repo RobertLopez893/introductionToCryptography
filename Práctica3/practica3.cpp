575e6a29513f06bddd452ad4433a867ab6b1570d
Robert <lopez.reyes.jose.m@gmail.com>
2025-09-19 16:13:16 -0600

    🤕Tested Affine Cipher

---
+++ Práctica3/practica3.cpp
@@ -0,0 +1,32 @@
+// Lab 03. Multiplicative Inverse
+// By: López Reyes José Roberto and Hernández Zamora Alejandro. 6CV4.
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <numeric>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+// Extended Euclidean Algorithm (EEA)
+vector<int> eea(int r0, int r1)
+{
+    int s0, t0 = 0;
+    int s1, t1, i = 1;
+
+    do
+    {
+    } while (r1 != 0);
+
+    return {};
+}
+
+int main()
+{
+    return 0;
+}
