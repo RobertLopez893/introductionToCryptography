57edfaf5d251a584d8e0c94928cab0838709f057
Robert <lopez.reyes.jose.m@gmail.com>
2025-10-08 19:07:34 -0600

    🤕Testing Perm CIpher

--- Práctica4/practica4.cpp
+++ Práctica4/practica4.cpp
@@ -4,14 +4,25 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <map>
-#include <numeric>
 #include <algorithm>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
+#include <random>
+#include <numeric>
 
 using namespace std;
+
+// Generates a Random Permutation (for testing purposes)
+vector<int> gen_perm(int n)
+{
+    vector<int> P(n);
+
+    iota(P.begin(), P.end(), 0);
+
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(P.begin(), P.end(), g);
+
+    return P;
+}
 
 // Computes the Inverse Permutation
 vector<int> inverse_permutation(vector<int> perm)
@@ -38,7 +49,12 @@ string perm_cipher(string M, vector<int> P)
 {
     string C;
 
-    // Fallos si la longitud de M no es igual a la de P
+    if (M.size() % P.size() != 0)
+    {
+        cout << "The message's and the permutation's length need to be multiples." << endl;
+        return "Error";
+    }
+
     for (int i = 0; i < M.size() / P.size(); i++)
     {
         int x = 0;
@@ -46,7 +62,7 @@ string perm_cipher(string M, vector<int> P)
         for (int j = 0; j < P.size(); j++)
         {
             x = P[j];
-            S += M[x];
+            S += M[(i * P.size()) + x];
         }
         C += S;
     }
@@ -56,15 +72,23 @@ string perm_cipher(string M, vector<int> P)
 
 int main()
 {
-    vector<int> P = {3, 1, 0, 2};
-    string M = "FLOR SIL";
+    int n = 6;
+    vector<int> P = gen_perm(n);
+
+    cout << "Permutation: [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << P[i] << " ";
+    }
+    cout << "]" << endl;
+
+    string M = "Mario! Bors?";
 
     string C = perm_cipher(M, P);
 
     cout << "Plaintext: " << M << endl;
     cout << "Ciphertext: " << C << endl;
 
-    C = "RLFO";
     P = inverse_permutation(P);
 
     M = perm_cipher(C, P);
